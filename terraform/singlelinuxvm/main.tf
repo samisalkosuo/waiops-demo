686286f839977f1c98506fcd1de576ba9fdb9b9f
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2023-01-10 09:56:28 +0200

    updated

--- terraform/singlelinuxvm/main.tf
+++ terraform/singlelinuxvm/main.tf
@@ -115,7 +115,7 @@ resource "vsphere_virtual_machine" "vm_1" {
   guest_id         = data.vsphere_virtual_machine.vm_1_template.guest_id
   scsi_type        = data.vsphere_virtual_machine.vm_1_template.scsi_type
   firmware         = var.vm_1-firmware
-  tags             = data.vsphere_tag.tag.*.id
+  tags             = vsphere_tag.tag.*.id
   clone {
     template_uuid = data.vsphere_virtual_machine.vm_1_template.id
 
